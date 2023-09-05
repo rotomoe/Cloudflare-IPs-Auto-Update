95bc32f25158db81dc2ddf789229d1235de04ed7
若瞳 <142956663+rotomoe@users.noreply.github.com>
2023-09-05 21:37:26 +0800

    Update update.sh

--- Linux-Shell/Nginx/update.sh
+++ Linux-Shell/Nginx/update.sh
@@ -2,9 +2,9 @@ curl -o v4.txt https://www.cloudflare.com/ips-v4
 curl -o v6.txt https://www.cloudflare.com/ips-v6
 
 sed 's/^/set_real_ip_from &/g' v4.txt > v4.conf.txt
-sed 's/$/&;/g' v4.conf.txt > v4.conf.new
+sed 's/$/&;/g' v4.conf.txt > v4.conf
 sed 's/^/set_real_ip_from &/g' v6.txt > v6.conf.txt
-sed 's/$/&;/g' v6.conf.txt > v6.conf.new
+sed 's/$/&;/g' v6.conf.txt > v6.conf
 
 rm -rf v4.txt
 rm -rf v4.conf.txt
